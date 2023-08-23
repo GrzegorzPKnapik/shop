37f751238c695eba624461e06957a56525813592
GrzegorzPKnapik <grzegorz.p.knapik@gmail.com>
2023-08-23 23:36:41 +0200

    blad

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,19 +11,16 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-**/
+|
+*/
 
 Route::get('/', function () {
-
-
-
     return view('welcome');
-
-
 });
+
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
+Route::get('/elel', function () {
+    return view('index');
+});
